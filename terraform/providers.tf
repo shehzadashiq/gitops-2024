4f0cbe71c5774bb509c6042ca89a1377a6f5e332
Shehzad <shehzadashiq@hotmail.com>
2024-10-13 22:29:54 +0100

    Address tflint errors
    Signed-off-by: Shehzad <shehzadashiq@hotmail.com>

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  region = var.region
-  # access_key = var.TF_VAR_AWS_ACCESS_KEY_ID
-  # secret_key = var.TF_VAR_AWS_SECRET_ACCESS_KEY
+  region     = var.region
+  access_key = var.TF_VAR_AWS_ACCESS_KEY_ID
+  secret_key = var.TF_VAR_AWS_SECRET_ACCESS_KEY
 }
