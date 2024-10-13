6be656060a0a143f9a766a8ef5568c63cbd7c40d
Shehzad <shehzadashiq@hotmail.com>
2024-10-13 22:14:47 +0100

    Remove TF_VARS in code
    Signed-off-by: Shehzad <shehzadashiq@hotmail.com>

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,12 +7,12 @@ variable "instance_type" {
   default = "t3.small"
 }
 
-variable "TF_VAR_AWS_ACCESS_KEY_ID" {
-  type      = string
-  sensitive = true
-}
+# variable "TF_VAR_AWS_ACCESS_KEY_ID" {
+#   type      = string
+#   sensitive = true
+# }
 
-variable "TF_VAR_AWS_SECRET_ACCESS_KEY" {
-  type      = string
-  sensitive = true
-}
+# variable "TF_VAR_AWS_SECRET_ACCESS_KEY" {
+#   type      = string
+#   sensitive = true
+# }
