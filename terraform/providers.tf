e31860e5c91fb578425967d419e77456d4b4cf9e
Shehzad <shehzadashiq@hotmail.com>
2024-10-21 17:50:43 +0100

    Add cloud configuration
    Signed-off-by: Shehzad <shehzadashiq@hotmail.com>

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -9,13 +9,13 @@ terraform {
   #   }
   # }
 
-  # cloud {
-  #   organization = "shehzad"
+  cloud {
+    organization = "shehzad"
 
-  #   workspaces {
-  #     name = "GitOps-2024"
-  #   }
-  # }
+    workspaces {
+      name = "GitOps-2024"
+    }
+  }
 
   required_providers {
     aws = {
