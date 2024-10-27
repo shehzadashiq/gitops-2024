bcf1fa7e026333824433083d66d2d5c574bb3575
Shehzad <shehzadashiq@hotmail.com>
2024-10-27 14:47:12 +0000

    Revert S3 backend change
    Signed-off-by: Shehzad <shehzadashiq@hotmail.com>

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,24 +1,24 @@
 terraform {
 
-  # cloud {
-  #   # The name of your Terraform Cloud organization.
-  #   organization = "shehzad"
+  cloud {
+    # The name of your Terraform Cloud organization.
+    organization = "shehzad"
 
-  #   # The name of the Terraform Cloud workspace to store Terraform state files in.
-  #   workspaces {
-  #     name = "GitOps-2024"
-  #   }
-  # }
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "GitOps-2024"
+    }
+  }
 
   # S3 configuration is not required as we are using a cloud configuration
   # Using both will generate the following error
   # Error: Both a backend and cloud configuration are present
-  backend "s3" {
-    bucket         = "gitops-tf-backend-shehzadashiq"
-    key            = "terraform.tfstate"
-    dynamodb_table = "GitopsTerraformLocks"
-    region         = "eu-west-2"
-  }
+  # backend "s3" {
+  #   bucket         = "gitops-tf-backend-shehzadashiq"
+  #   key            = "terraform.tfstate"
+  #   dynamodb_table = "GitopsTerraformLocks"
+  #   region         = "eu-west-2"
+  # }
 
   required_providers {
     aws = {
